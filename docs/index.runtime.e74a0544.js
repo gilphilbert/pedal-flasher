e7b599f4da9abdc69288347f57d6b261af407d4e
gilphilbert <gilphilbert@protonmail.com>
2024-11-04 10:08:10 -0800

    readd

---
+++ docs/index.runtime.e74a0544.js
@@ -0,0 +1,68 @@
+
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+      var $parcel$global = globalThis;
+    
+var $parcel$modules = {};
+var $parcel$inits = {};
+
+var parcelRequire = $parcel$global["parcelRequire477f"];
+
+if (parcelRequire == null) {
+  parcelRequire = function(id) {
+    if (id in $parcel$modules) {
+      return $parcel$modules[id].exports;
+    }
+    if (id in $parcel$inits) {
+      var init = $parcel$inits[id];
+      delete $parcel$inits[id];
+      var module = {id: id, exports: {}};
+      $parcel$modules[id] = module;
+      init.call(module.exports, module, module.exports);
+      return module.exports;
+    }
+    var err = new Error("Cannot find module '" + id + "'");
+    err.code = 'MODULE_NOT_FOUND';
+    throw err;
+  };
+
+  parcelRequire.register = function register(id, init) {
+    $parcel$inits[id] = init;
+  };
+
+  $parcel$global["parcelRequire477f"] = parcelRequire;
+}
+
+var parcelRegister = parcelRequire.register;
+parcelRegister("aKzDW", function(module, exports) {
+
+$parcel$export(module.exports, "register", () => $7d39d93f9098a310$export$6503ec6e8aabbaf, (v) => $7d39d93f9098a310$export$6503ec6e8aabbaf = v);
+$parcel$export(module.exports, "resolve", () => $7d39d93f9098a310$export$f7ad0328861e2f03, (v) => $7d39d93f9098a310$export$f7ad0328861e2f03 = v);
+var $7d39d93f9098a310$export$6503ec6e8aabbaf;
+var $7d39d93f9098a310$export$f7ad0328861e2f03;
+"use strict";
+var $7d39d93f9098a310$var$mapping = new Map();
+function $7d39d93f9098a310$var$register(baseUrl, manifest) {
+    for(var i = 0; i < manifest.length - 1; i += 2)$7d39d93f9098a310$var$mapping.set(manifest[i], {
+        baseUrl: baseUrl,
+        path: manifest[i + 1]
+    });
+}
+function $7d39d93f9098a310$var$resolve(id) {
+    var resolved = $7d39d93f9098a310$var$mapping.get(id);
+    if (resolved == null) throw new Error("Could not resolve bundle with id " + id);
+    return new URL(resolved.path, resolved.baseUrl).toString();
+}
+$7d39d93f9098a310$export$6503ec6e8aabbaf = $7d39d93f9098a310$var$register;
+$7d39d93f9098a310$export$f7ad0328861e2f03 = $7d39d93f9098a310$var$resolve;
+
+});
+
+var $0f0d3a9ccebc3fdf$exports = {};
+
+(parcelRequire("aKzDW")).register(new URL("", import.meta.url).toString(), JSON.parse('["2YPXu","index.ceb9099e.js","haz8S","esp32.1d80f456.js","gzQcJ","esp32c2.7b879c2d.js","c56MU","esp32c3.d1edf56a.js","eiUVR","esp32c6.a3b1aea3.js","gjFl0","esp32c61.87bfe5c0.js","5dFms","esp32c5.0ba37815.js","1a402","esp32h2.edef812a.js","8Hlav","esp32s3.af42c53d.js","9kojZ","esp32s2.94770d6d.js","7l8IP","esp8266.e2477f06.js","c6BRR","esp32p4.f421a0e9.js"]'));
+
+
+//# sourceMappingURL=index.runtime.e74a0544.js.map
